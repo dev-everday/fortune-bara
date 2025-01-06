d4f295f93c8c2707c1f747c27a59cfd7889ee32c
dev-everyday <everyharus@gmail.com>
2025-01-06 14:26:40 +0900

    Initialize Spring Boot project

---
+++ backend/src/main/java/com/fortunebara/fortune_bara_backend/FortuneBaraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fortunebara.fortune_bara_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneBaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneBaraBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fortunebara/fortune_bara_backend/FortuneBaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortunebara.fortune_bara_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneBaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
